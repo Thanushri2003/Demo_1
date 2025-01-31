9ebed72d7e74a3d35570e31106a580242c932291
Thanushri2003 <arivuselvi784@gmail.com>
2025-01-31 12:40:38 +0530

    happy

---
+++ src/main/java/com/mphasis/Demo1/App.java
@@ -0,0 +1,31 @@
+package com.mphasis.Demo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public void add(int a,int b)
+	{
+		int s;
+		s=a+b;
+		System.out.println("sum:"+s);
+	}
+	public void addData(String data)
+	{
+		if(data.equals("thanu"))
+		{System.out.println("welcome");
+		}
+		else {
+			System.out.println("byee");
+		}
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
+///
+
